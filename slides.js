546d38390572bfc7f09b23fd99558007978165f6
Kerry Buckley <kerryjbuckley@gmail.com>
2012-02-05 21:13:14 +0000

    Use sprites for multi-image slides

--- slides.js
+++ slides.js
@@ -5,36 +5,37 @@ $(document).ready(function(){
 var Presentation = {
   slides: [
     {background: "#ffff00", foreground: "#0000ff", inset: "highlight", content: "#title"},
-    {foreground: "#d7608e", inset: "shadow", letter: "A", images: ["activesupport"]},
-    {foreground: "#ff0000", inset: "shadow", letter: "B", images: ["blocks"]},
-    {foreground: "#0000ff", inset: "shadow", letter: "C", images: ["community", "coffeescript"]},
-    {foreground: "#ff00ff", inset: "shadow", letter: "D", images: ["duck_typing", "dhh"]},
-    {foreground: "#8888ff", inset: "highlight", letter: "E", images: ["enumerable"]},
-    {foreground: "#cc00ff", inset: "shadow", letter: "F", images: ["functional"]},
-    {foreground: "#d12f2d", inset: "shadow", letter: "G", images: ["gems"]},
-    {foreground: "#0000ff", inset: "shadow", letter: "H", images: ["happiness"]},
-    {foreground: "#ff8800", inset: "shadow", letter: "I", images: ["irb"]},
-    {foreground: "#f13e3e", inset: "shadow", letter: "J", images: ["java", "jruby"]},
-    {foreground: "#ffff00", inset: "shadow", letter: "K", images: ["kitten"]},
-    {foreground: "#4400ff", inset: "shadow", letter: "L", images: ["licence"]},
-    {foreground: "#d5b87c", inset: "shadow", letter: "M", images: ["magic", "matz"]},
+    {foreground: "#d7608e", inset: "shadow", letter: "A", images: 1},
+    {foreground: "#ff0000", inset: "shadow", letter: "B", images: 1},
+    {foreground: "#0000ff", inset: "shadow", letter: "C", images: 2},
+    {foreground: "#ff00ff", inset: "shadow", letter: "D", images: 2},
+    {foreground: "#8888ff", inset: "highlight", letter: "E", images: 1},
+    {foreground: "#cc00ff", inset: "shadow", letter: "F", images: 1},
+    {foreground: "#d12f2d", inset: "shadow", letter: "G", images: 1},
+    {foreground: "#0000ff", inset: "shadow", letter: "H", images: 1},
+    {foreground: "#ff8800", inset: "shadow", letter: "I", images: 1},
+    {foreground: "#f13e3e", inset: "shadow", letter: "J", images: 2},
+    {foreground: "#ffff00", inset: "shadow", letter: "K", images: 1},
+    {foreground: "#4400ff", inset: "shadow", letter: "L", images: 1},
+    {foreground: "#d5b87c", inset: "shadow", letter: "M", images: 2},
     {background: "rgba(255, 255, 255, 0.7)", foreground: "#000000", inset: "highlight", letter: "N", content: "#nil"},
-    {foreground: "#ff0000", inset: "shadow", letter: "O", images: ["open"]},
-    {foreground: "#f36118", inset: "shadow", letter: "P", images: ["mr_fussy", "pragmatic"]},
-    {foreground: "#af282a", inset: "shadow", letter: "Q", images: ["quiz"]},
-    {foreground: "#4444ff", inset: "highlight", letter: "R", images: ["rake", "rack", "rvm", "rails", "rspec", "reminder", "toolbox"]},
-    {foreground: "#2b423f", inset: "shadow", letter: "S", images: ["library"]},
-    {background: "#cccccc", foreground: "#2e5d67", inset: "highlight", letter: "T", images: ["tdd"]},
-    {foreground: "#dddfdf", inset: "shadow", letter: "U", images: ["obiwan"]},
-    {background: "#58aaf7", foreground: "#d6471c", inset: "highlight", letter: "V", images: ["vim"]},
-    {foreground: "#ff0000", inset: "highlight", letter: "W", images: ["why"]},
-    {foreground: "#f33f57", inset: "shadow", letter: "X", images: ["xml"]},
-    {foreground: "#ff8800", inset: "highlight", letter: "Y", images: ["young"]},
-    {foreground: "white", inset: "shadow", letter: "Z", images: ["zero"]},
+    {foreground: "#ff0000", inset: "shadow", letter: "O", images: 1},
+    {foreground: "#f36118", inset: "shadow", letter: "P", images: 2},
+    {foreground: "#af282a", inset: "shadow", letter: "Q", images: 1},
+    {foreground: "#4444ff", inset: "highlight", letter: "R", images: 7},
+    {foreground: "#2b423f", inset: "shadow", letter: "S", images: 1},
+    {foreground: "#2e5d67", inset: "highlight", letter: "T", images: 1},
+    {foreground: "#dddfdf", inset: "shadow", letter: "U", images: 1},
+    {foreground: "#d6471c", inset: "highlight", letter: "V", images: 1},
+    {foreground: "#ff0000", inset: "highlight", letter: "W", images: 1},
+    {foreground: "#f33f57", inset: "shadow", letter: "X", images: 1},
+    {foreground: "#ff8800", inset: "highlight", letter: "Y", images: 1},
+    {foreground: "white", inset: "shadow", letter: "Z", images: 1},
     {background: "#cccccc", foreground: "#444444", content: "#credits"}
   ],
 
   current_slide: 0,
+  current_letter: "",
   current_image: 0,
   current_face: "front",
   notes_showing: false,
@@ -64,17 +65,13 @@ var Presentation = {
     $("#notes-0").show();
     $("#toggle-notes").click(Presentation.toggle_notes);
     $("#notes a").attr("target", "ruby_a_to_z_links");
-    $(document).keydown(function(e) {
-      Presentation.handle_key_down(e.which);
-    }).keypress(function(e) {
-      Presentation.handle_key_press(e.which);
-    });
+    $(document).keydown(Presentation.handle_key_down).keypress(Presentation.handle_key_press);
     this.cube.populate();
     this.show_initial_slide();
   },
 
   preload_images: function() {
-    images = $.map(this.slides.map(function(s) {return s.images;}), function(a) {return a;});
+    var images = $.map(this.slides.map(function(s) {if (s.images) {return s.letter;}}), function(a) {return a;});
     images.forEach(function(i) { (new Image()).src = "images/" + i + ".png"; });
   },
 
@@ -105,21 +102,28 @@ var Presentation = {
     }
   },
 
-  handle_key_down: function(key) {
+  handle_key_down: function(event) {
+    key = event.which;
     if (key === 39 || key === 40) {
-      this.next();
+      event.preventDefault();
+      Presentation.next();
     } else if (key === 37 || key === 38) {
-      this.previous();
+      event.preventDefault();
+      Presentation.previous();
     }
   },
 
-  handle_key_press: function(key) {
+  handle_key_press: function(event) {
+    key = event.which;
     if (key === 32 || key === 13) {
-      this.next();
+      event.preventDefault();
+      Presentation.next();
     } else if (key >= 97 && key <= 122) {
-      this.go_to_slide(key - 96);
+      event.preventDefault();
+      Presentation.go_to_slide(key - 96);
     } else if (key === 48) {
-      this.go_to_slide(0);
+      event.preventDefault();
+      Presentation.go_to_slide(0);
       $("#cube").removeClass().addClass("initial");
     }
   },
@@ -127,12 +131,13 @@ var Presentation = {
   next: function() {
     function has_another_image() {
       return Presentation.slides[Presentation.current_slide].images &&
-        Presentation.slides[Presentation.current_slide].images.length > Presentation.current_image + 1;
+        Presentation.slides[Presentation.current_slide].images > Presentation.current_image + 1;
     }
 
     if (has_another_image()) {
       this.current_image += 1;
-      this.change_image();
+      var face = this.face_element_named(this.current_face);
+      face.css("background-position", "-" + (this.current_image * 600) + "px 0");
     } else {
       this.current_image = 0;
       this.go_to_slide(this.current_slide + 1);
@@ -151,10 +156,8 @@ var Presentation = {
   },
 
   change_image: function() {
-    var images = this.slides[this.current_slide].images;
-    if (images) {
-      this.display_image(this.face_element_named(this.current_face),
-          images[this.current_image]);
+    if (this.slides[this.current_slide].images) {
+      this.display_image(this.face_element_named(this.current_face), this.current_image);
       location.hash = this.current_slide + "." + this.current_image;
     }
   },
@@ -169,8 +172,8 @@ var Presentation = {
         face_name = face.name;
       } else {
         face_name = this.random_other_face();
-        this.display_slide(face_name, number);
       }
+      this.display_slide(face_name, number);
       $("#cube").removeClass().addClass("show-" + face_name);
       this.current_slide = number;
       this.current_face = face_name;
@@ -195,6 +198,7 @@ var Presentation = {
     }
 
     var slide = this.slides[number];
+    this.current_letter = slide.letter;
     var face = this.face_element_named(face_name);
     this.cube.face_named(face_name).slide = number;
     if (slide.content) {
@@ -206,7 +210,7 @@ var Presentation = {
     face.css("color", slide.foreground);
     if (slide.background) { face.css("background-color", slide.background); }
     if (slide.images) {
-      this.display_image(face, slide.images[0]);
+      this.display_image(face, 0);
       this.current_image = 0;
     } else {
       this.remove_image(face);
@@ -219,8 +223,9 @@ var Presentation = {
     }
   },
 
-  display_image: function(face, image) {
-    face.css("background-image", "url('images/" + image + ".png')");
+  display_image: function(face, number) {
+    face.css("background-image", "url('images/" + this.current_letter + ".png')");
+    face.css("background-position", "0 0");
   },
 
   remove_image: function(face) {
